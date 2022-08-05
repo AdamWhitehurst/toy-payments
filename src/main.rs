21d9153e579e0d4ab96375fbfe14912878881a8f
Adam Whitehurst <adam.whitehurst@live.com>
2022-08-05 07:35:30 -0700

    feat(serde): csv rows into type

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,43 @@ use std::error::Error;
 use std::ffi::OsString;
 use std::fs::File;
 use std::process;
+use serde::Deserialize;
+
+// TODO: Organize code in files
+// TODO: Add all record types to transasctions.csv
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+enum TransactionType {
+    Deposit,
+    Withdrawal,
+    Dispute,
+    Resolve,
+    Chargeback,
+}
+
+#[derive(Debug, Deserialize)]
+struct TransactionRecord {
+    #[serde(rename = "type")]
+    tx_type: TransactionType,
+    #[serde(rename = "client")]
+    client_id: u16,
+    #[serde(rename = "tx")]
+    tx_id: u32,
+    #[serde(rename = "amount")]
+    #[serde(deserialize_with = "csv::invalid_option")]
+    amount: Option<f32>,
+}
 
 fn run() -> Result<(), Box<dyn Error>> {
     let file_path = get_first_arg()?;
     let file = File::open(file_path)?;
-    let mut rdr = csv::Reader::from_reader(file);
-    for result in rdr.records() {
-        let record = result?;
-        println!("{:?}", record);
+    // let mut rdr = csv::Reader::from_reader(file);
+    let mut rdr = csv::ReaderBuilder::new()
+    .trim(csv::Trim::All).from_reader(file);
+    for result in rdr.deserialize() {
+        let record: TransactionRecord = result?;
+        println!("{:#?}", record);
     }
     Ok(())
 }
