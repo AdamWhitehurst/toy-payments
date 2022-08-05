a08a5c8a0fc69fe5efcda2f271213ca9265fe90b
Adam Whitehurst <adam.whitehurst@live.com>
2022-08-05 10:11:43 -0700

    feat(ledger): stub tx parsing

--- src/main.rs
+++ src/main.rs
@@ -44,33 +44,77 @@ struct Account {
 struct Ledger {
     /// Maps between client ID's and their corresponding accounts
     accounts: HashMap<u16, Account>,
+    // Maps between transaction ID's and their corresponding record
+    txs: HashMap<u32, TransactionRecord>,
 }
 
 impl Ledger {
     fn new() -> Self {
         Ledger {
             accounts: HashMap::new(),
+            txs: HashMap::new(),
         }
     }
+    fn parse_transaction(&mut self, r: TransactionRecord) {
+        use TransactionType::*;
+        match r.tx_type {
+            Deposit => self.deposit(r),
+            Withdrawal => self.withdrawal(r),
+            Dispute => self.dispute(r),
+            Resolve => self.resolve(r),
+            Chargeback => self.chargeback(r),
+        }
+    }
+
     fn fold_transaction(mut self, r: TransactionRecord) -> Self {
-        println!("{:#?}", r);
+        self.parse_transaction(r);
         self
+    }
+
+    fn deposit(&mut self, r: TransactionRecord) {
+        println!("{:#?}", r);
+    }
+
+    fn withdrawal(&mut self, r: TransactionRecord) {
+        println!("{:#?}", r);
+    }
+
+    fn dispute(&mut self, r: TransactionRecord) {
+        println!("{:#?}", r);
+    }
+
+    fn resolve(&mut self, r: TransactionRecord) {
+        println!("{:#?}", r);
+    }
+
+    fn chargeback(&mut self, r: TransactionRecord) {
+        println!("{:#?}", r);
     }
 }
 
 fn run() -> Result<(), Box<dyn Error>> {
+    // Try to read file from arg name
     let file_path = get_first_arg()?;
     let file = File::open(file_path)?;
+    // Make a CSV reader
     let mut rdr = csv::ReaderBuilder::new()
+        // Support spaces between fields
         .trim(csv::Trim::All)
         .from_reader(file);
+    // Compose a Ledger from csv lines
     let ldgr = rdr
         .deserialize()
+        // Only valid tx's
         .filter_map(into_tx)
+        // Parse each record into ledger
         .fold(Ledger::new(), |acc, r| acc.fold_transaction(r));
+
+    // TODO: Serialize Ledger
+
     Ok(())
 }
 
+/// Converts Results into Options
 fn into_tx(r: Result<TransactionRecord, csv::Error>) -> Option<TransactionRecord> {
     // Error handling could happen here
     r.ok()
